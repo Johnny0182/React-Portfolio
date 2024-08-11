37aec79a05cf7e6b140610c5020e9e3518b19115
Johnny Leyva <johnny.leyva182@gmail.com>
2024-08-11 14:21:17 -0700

    deployment setup

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   // experimental: {
   //   serverActions: true,
   // },
-  output: 'standalone', // This is important for Netlify deployment
+  output: 'standalone', // This is important for Netlify dgieployment
 };
 
 module.exports = nextConfig;
