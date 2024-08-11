69ceeaad55da8a7362166583d7c342a05900169e
Johnny Leyva <johnny.leyva182@gmail.com>
2024-08-11 14:18:43 -0700

    deployment issue fixing

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.imgur.com',
-        },
-      ],
-    },
-    experimental: {
-      serverActions: true,
-    },
-  };
-  
-module.exports = nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
+  // Remove the experimental section as server actions are now available by default
+  // experimental: {
+  //   serverActions: true,
+  // },
+  output: 'standalone', // This is important for Netlify deployment
+};
 
-  
+module.exports = nextConfig;
