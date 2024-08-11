3db0977e35a7f7564a4ffaa3e540f04721abba2f
Johnny Leyva <johnny.leyva182@gmail.com>
2024-08-11 10:16:22 -0700

    Completed the contact form, added toast for style, and error messages.

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react';
 import SectionHeading from './section-heading';
-import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from './submit-button';
+import toast from 'react-hot-toast';
 
 
 export default function Contact() {
@@ -43,7 +44,14 @@ export default function Contact() {
 
       <form className="mt-10 flex flex-col"
       action={async (formData) => {
-        await sendEmail(formData);
+        const { data, error } = await sendEmail(formData);
+        
+        if (error) {
+          toast.error(error);
+          return;
+        }
+        toast.success('Email sent successfully! : )');
+        // goal: add a confetti animation!
       }} 
       >
         <input 
@@ -61,15 +69,7 @@ export default function Contact() {
         required
         maxLength={5000} 
         />
-        <button type="submit" 
-        className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-950
-        text-white rounded-full outline-none transition-all
-        focus:scale-110 hover:scale-110 active:scale-105 hover:bg-blue-700">
-            Submit <FaPaperPlane
-            className="text-xs opacity-80 transition-all
-            group-hover:tanslate-x-1
-            group-hover:-translate-y-1" />{""}
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
