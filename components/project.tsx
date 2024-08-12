a7ed9291289ed73bbf16d42ae84b5b9fa8cb7952
Johnny Leyva <johnny.leyva182@gmail.com>
2024-08-12 06:38:38 -0700

    fixing view on iphone 13 mini : )

--- components/project.tsx
+++ components/project.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from 'next/image';
@@ -18,8 +19,8 @@ export default function Project({
         target: ref,
         offset: ["0 1", "1.33 1"]
     });
-    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
-    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
     return (
         <motion.div
@@ -31,20 +32,22 @@ export default function Project({
             className="group mb-3 sm:mb-8 last:mb-0"
         >
             <section className="bg-gray-100 max-w-[42rem] border border-black/5
-                overflow-hidden sm:pr-8 relative sm:h-[20rem] group-even:pl-8
+                overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8
                 hover:bg-gray-200 transition rounded-lg">
                 <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] 
                 flex flex-col h-full group-even:ml-[18rem]"
                 >
-                <h3 className="text-2xl font-semibold">
-                {title}</h3>
+                <h3 className="text-2xl font-semibold">{title}</h3>
                 <p className="mt-2 leading-relaxed text-gray-700">
-                {description}</p>
+                {description}
+                </p>
                     <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
                     {tags.map((tag, index) => (
                     <li
                     className="bg-black/[0.7] px-3 py-1 text-[0.7rem] 
-                    uppercase tracking-wider text-white rounded-full" key={index}>{tag}
+                    uppercase tracking-wider text-white rounded-full" key={index}
+                    >
+                        {tag}
                     </li>
                 ))}
                     </ul>
@@ -54,8 +57,7 @@ export default function Project({
             <Image src={imageUrl} 
             alt="Projects I worked on!"
             quality={95}
-            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg
-            shadow-2xl
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
             transition
             group-hover:scale-[1.04]
             group-hover:-translate-x-3
