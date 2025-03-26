9e2a9e71be200dad65fce84418dc2a7f7d42ef25
Johnny Leyva <johnny.leyva182@gmail.com>
2025-03-26 00:39:24 -0700

    spotify player addition

--- components/contact.tsx
+++ components/contact.tsx
@@ -73,6 +73,17 @@ export default function Contact() {
         />
         <SubmitBtn />
       </form>
+        <iframe
+          style={{ borderRadius: '12px' }}
+          src="https://open.spotify.com/embed/track/700dnb47ud1w4fojNpIjxD?utm_source=generator"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          allowFullScreen
+          className="mt-10 shadow-lg"
+        />
     </motion.section>
   );
 }
